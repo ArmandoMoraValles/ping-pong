60f6f175b0de5c03f2e3b7333cca6bcb47b79f86
ArmadoMoraValles <armandomoravalles@gmail.com>
2022-12-02 05:38:59 -0600

    build(backend): Add the initial configuration of the project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+console.clear()
+
+app.get('/', (req,res) => res.send('Hello World!'))
+
+app.use('/api', require('./routes/firstExample.js'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
